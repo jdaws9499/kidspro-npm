90c812851b8c03f4d47f1c5f0bf38bbc35104b3a
Juehee Dawson <jdaws087@uottawa.ca>
2023-04-14 12:42:05 -0400

    using pkijs to parse certificate and reading extensions now.

--- src/background.js
+++ src/background.js
@@ -1,15 +1,86 @@
-'use strict';
+import { Certificate } from 'pkijs';
+import * as asn1js from 'asn1js';
+//'use strict';
 
 // With background scripts you can communicate with popup
 // and contentScript files.
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+/**
+ * @param {*} extensions 
+ * @param {*} id 
+ * @returns 
+ */
+function getX509Ext(extensions, id) {
+  for (var extension in extensions) {
+    if (extensions[extension].extnID === id) {
+      return extensions[extension];
+    }
+  }
+}
+
+async function getCertificateOnCurrentTab(details) {
+  try {
+    console.log('newcert - ' + JSON.stringify(details));
+    let securityInfo = await browser.webRequest.getSecurityInfo(
+      details.requestId,
+      { "certificateChain": true, rawDER: true }
+    );
+    if (securityInfo) {
+      let cert = securityInfo.certificates[0];
+      console.log('certificate - ' + JSON.stringify(cert));
+      let certificateChain = new Uint8Array(cert.rawDER).buffer;
+      let asn1 = asn1js.fromBER(certificateChain);
+
+      let x509 = await new Certificate({ schema: asn1.result });
+      if (x509) {
+        x509 = x509.toJSON();
+        //console.log('x509 in pkijs - ' + x509);
+        console.log('extensions - ' + JSON.stringify(x509.extensions));
+        let san = getX509Ext(x509.extensions, '2.5.29.17').parsedValue;
+        if (san && san.hasOwnProperty('altNames')) {
+          console.log('*Subject Alt Names: ' + JSON.stringify(san));
+        }
+        let kidsRating = getX509Ext(x509.extensions, "1.2.3.4");
+        if (kidsRating) {
+          console.log('*kidsRating: ' + JSON.stringify(kidsRating));
+        }
+      }
+    }
+
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+browser.webRequest.onHeadersReceived.addListener(
+  details => {
+    getCertificateOnCurrentTab(details);
+  },
+  { urls: ["<all_urls>"], types: ["main_frame"] },
+  ["blocking"]
+)
+
+browser.webRequest.onErrorOccurred.addListener(
+  details => {
+    // eventually we will be able to consume these details, but for now we can only
+    // disable the icon
+    // consume(details);
+    if (details.type === 'main_frame' && details.documentUrl === undefined) {
+      console.log('error happend' + JSON.stringify(details));
+      //icon.update(details.tabId, 'http');
+    }
+  },
+  { urls: ['<all_urls>'] }
+);
+
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+    const message = `Hi ${sender.tab ? 'Con' : 'Pop'
+      }, my name is Bac. I am from Background. It's great to hear from you.`;
 
     // Log message coming from the `request` parameter
     console.log(request.payload.message);
